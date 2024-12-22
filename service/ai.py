1fe8352b683129f55d9ab090b56e4a3dcac01701
Joshua Cerdenia <joshcerdenia@gmail.com>
2024-12-22 18:45:21 +0800

    refactor(ai): fetch prompt only once per run

--- service/ai.py
+++ service/ai.py
@@ -10,9 +10,16 @@ class AIService:
         prompt_id = os.getenv("PROMPT_DOCUMENT_ID")
         self.docs = DocScraper(prompt_id, "BEGIN PROMPT")
 
+        self._set_base_prompt()
+
+    def _set_base_prompt(self):
+        self.base_prompt = self.docs.get_content()
+
     def _get_prompt(self, data):
-        base_prompt = self.docs.get_content()
-        return f"{base_prompt} {data}"
+        if not self.base_prompt:
+            self._set_base_prompt()
+
+        return f"{self.base_prompt} {data}"
 
     def write_article(self, data):
         prompt = self._get_prompt(data)
