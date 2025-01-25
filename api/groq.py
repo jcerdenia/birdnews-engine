a21df5f926fce37b4fa93a2e71952c4cbb59e0f7
Joshua Cerdenia <joshcerdenia@gmail.com>
2025-01-25 17:52:43 +0800

    feat(groq): update model

--- api/groq.py
+++ api/groq.py
@@ -5,16 +5,17 @@ from misc.decorators import handle_error
 
 class GroqAPI:
     ROLE_USER = "user"
-    MODEL_DEFAULT = "llama3-8b-8192"
+    MODEL_LLAMA3_8B = "llama-3.1-8b-instant"
+    MODEL_LLAMA3_70B = "llama3-70b-8192"
 
     def __init__(self, api_key):
         self.client = Groq(api_key=api_key)
 
     @handle_error
-    def chat(self, prompt):
+    def chat(self, prompt, model=MODEL_LLAMA3_8B):
         chat_completion = self.client.chat.completions.create(
             messages=[{"role": self.ROLE_USER, "content": prompt}],
-            model=self.MODEL_DEFAULT,
+            model=model,
         )
 
         return chat_completion.choices[0].message.content
