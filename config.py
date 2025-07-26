cbdd6f49f7239dc36cdfc52534f79af0c267ad91
Joshua Cerdenia <joshua.cerdenia@firstdigitalfinance.com>
2025-07-26 13:15:10 +0800

    refactor(config): declare env vars at class level

--- config.py
+++ config.py
@@ -4,32 +4,38 @@ from dotenv import load_dotenv
 
 
 class Config:
-    ENV_VARS = [
-        "EBIRD_API_KEY",
-        "EBIRD_REGION_CODE",
-        "GROQ_API_KEY",
-        "SANITY_API_KEY",
-        "SANITY_PROJECT_ID",
-        "BREVO_API_KEY",
-        "BREVO_RECIPIENT_LIST_ID",
-        "SPREADSHEET_ID",
-        "EMAIL_WORKSHEET_IDX",
-        "PROMPT_WORKSHEET_IDX",
-        "CHECKLIST_WORKSHEET_IDX",
-        "API_TOKEN",
-        "TZ",
-    ]
+    EBIRD_API_KEY = None
+    EBIRD_REGION_CODE = None
+    GROQ_API_KEY = None
+    SANITY_API_KEY = None
+    SANITY_PROJECT_ID = None
+    BREVO_API_KEY = None
+    BREVO_RECIPIENT_LIST_ID = None
+    SPREADSHEET_ID = None
+    EMAIL_WORKSHEET_IDX = None
+    PROMPT_WORKSHEET_IDX = None
+    CHECKLIST_WORKSHEET_IDX = None
+    API_TOKEN = None
+    TZ = None
 
     def __init__(self):
         load_dotenv()
 
-        for var in self.ENV_VARS:
+        for var in self.get_env_vars():
             value = os.environ[var]
 
             if value.isnumeric():
                 value = int(value)
 
             setattr(self, var, value)
+
+    @classmethod
+    def get_env_vars(cls):
+        return [
+            key
+            for key, value in vars(cls).items()
+            if not key.startswith("_") and value is None
+        ]
 
     @classmethod
     def from_env(cls):
